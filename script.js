c2f4a04dbba7899d2d218b37953d5cab76ae1b25
Minteckk <s10228079@connect.np.edu.sg>
2022-02-06 00:43:26 +0800

    add javascript function call

---
+++ script.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    const APIKEY = "61fe3faf6a791555010217e6";
+
+// Get the modal
+function SignUpForm() {
+var modal = document.getElementById('SignupModal');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
+    // api
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://assignmentapi-7328.restdb.io/rest/students",
+    "method": "GET",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "APIKEY",
+      "cache-control": "no-cache"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+
+var jsondata = {"field1": "xyz","field2": "abc"};
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://assignmentapi-7328.restdb.io/rest/students",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json",
+    "x-apikey": "APIKEY",
+    "cache-control": "no-cache"
+  },
+  "processData": false,
+  "data": JSON.stringify(jsondata)
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var jsondata = {"field1": "new value","field2": "xxx"};
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://assignmentapi-7328.restdb.io/rest/students/{id}",
+  "method": "PUT",
+  "headers": {
+    "content-type": "application/json",
+    "x-apikey": "APIKEY",
+    "cache-control": "no-cache"
+  },
+  "processData": false,
+  "data": JSON.stringify(jsondata)
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+})
