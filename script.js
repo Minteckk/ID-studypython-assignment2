d85c57b23f98ab0946d1a7c88b017aba431906a1
Minteckk <s10228079@connect.np.edu.sg>
2022-02-14 15:39:21 +0800

    js for contact form and button

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// This function is used to let the current fields the user is typing in to get focused by styling the background colour.
+function infocus(x) {
+  x.style.background = "orange";
+  }
+  
+  // This function is used to focus out the current fields that being focus and move the focus to the next fields.
+  function outfocus(x) {
+    var x = document.getElementById("name");
+    x.value = x.value.toUpperCase();
+    x.style.background = "white";
+  }
+  
+  // This function is used to focus out the current fields that being focus and move the focus to the next fields.
+  function outfocusEmail(x) {
+    var x = document.getElementById("emailAddr");
+    x.value = x.value.toUpperCase();
+    x.style.background = "white";
+  }
+  
+  // This function is used to focus out the current fields that being focus and move the focus to the next fields.
+  function outfocusContact(x) {
+    var x = document.getElementById("contact");
+    x.value = x.value.toUpperCase();
+    x.style.background = "white";
+  }
+  
+  // This function is used to focus out the current fields that being focus and move the focus to the next fields.
+  function outfocustextarea(x) {
+    var x = document.getElementById("enquiries");
+    x.value = x.value.toUpperCase();
+    x.style.background = "white";
+  }
+
+function myFunction() {
+    location.replace("tutorial.html")
+  }
 
 $(document).ready(function () {
     
